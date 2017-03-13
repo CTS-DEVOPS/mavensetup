3a7b9d3ad352b1393b1340ef8716b7b085d766d6
ashwani <ashwani.ashwani.champurkar@gmail.com>
2017-03-13 11:31:21 +0530

    maven

---
+++ maventestapp/src/main/java/org/ashwani/java/App.java
@@ -0,0 +1,13 @@
+package org.ashwani.java;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "My name is ashwani!" );
+    }
+}
